6edf10c350221bfbde14b70616f03675caa644aa
kootee <katja.toivola@aalto.fi>
2019-04-05 14:23:52 +0300

    Update maiden_tiedot_App.js

--- maiden_tiedot_App.js
+++ maiden_tiedot_App.js
@@ -13,57 +13,61 @@ const Filter = (props) => {
   )
 }
 
-const ShowCountries = ({countriesList}) => {
-  if (countriesList.length > 10) {
+const ShowCountries = ({filteredList}) => {
+  if (filteredList.length > 10) {
     return (
       <p>too many matches, specify another filter</p>
     )
-  } else if (countriesList.length === 1 ) {
+  } else if (filteredList.length === 1 ) {
     return (
     <div>
-      <h2>{countriesList.map(country => <p key={country.name}>{country.name}</p>)}</h2>
-      <p>capital {countriesList.map(i => i.capital)}</p>
-      <p>population {countriesList.map(i => i.population)}</p>
+      <h2>{filteredList.map(country => <p key={country.name}>{country.name}</p>)}</h2>
+      <p>capital {filteredList.map(i => i.capital)}</p>
+      <p>population {filteredList.map(i => i.population)}</p>
       <h3>languages</h3>
       <ul>
-        {countriesList.map(i => i.languages.map(j => <li key={j.name}>{j.name}</li>))}
+        {filteredList.map(i => i.languages.map(j => <li key={j.name}>{j.name}</li>))}
       </ul>
       <picture>
-        <img src= {countriesList.map(i => i.flag)} alt='flag' style={{maxHeight: 150}}></img>
+        <img src= {filteredList.map(i => i.flag)} alt='flag' style={{maxHeight: 150}}></img>
       </picture>
     </div>
     )
   } else {
     return (
-      countriesList.map(country => <p key={country.name}>{country.name}</p>)
+      filteredList.map(country => <p key={country.name}>{country.name}</p>)
     )
   }
 }
 
 const App = () => {
  const [countriesList, setCountries] = useState([])
- const [query, setQuery] = useState('Finland') 
+ const [query, setQuery] = useState('Fin') 
 
   useEffect(() => {
     console.log('effect')
-    axios.get(`https://restcountries.eu/rest/v2/name/${query}`)
+    axios.get('https://restcountries.eu/rest/v2/all')
       .then(response => {
         console.log('promise fulfilled')
         setCountries(response.data)
     })
-  }, [query])
+  }, [])
 
   const handleFilterChange = (event) => {
     setQuery(event.target.value)
     console.log('new search term:', query)
   }
+
+  const filteredList = countriesList.filter(country => country.name.includes(query))
   
   console.log(countriesList)
+  console.log(filteredList)
+  
   return (
     <div>
       <h1>look for countries</h1>
       <Filter value={query} handleInput={handleFilterChange}/>
-      <ShowCountries countriesList={countriesList} />
+      <ShowCountries filteredList={filteredList} />
     </div>
   )
 }
